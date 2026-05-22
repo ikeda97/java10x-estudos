fad9067bd8ae7ee8511d934a1fa8f18cfda41872
ikeda97 <email@que@voce@usa@no@github.com>
2026-05-22 18:53:39 -0300

    Aula de dados nao primitivos

---
+++ 01-iniciante/src/TiposDeDados/DadosNaoPrimitivos.java
@@ -0,0 +1,23 @@
+package TiposDeDados;
+
+import java.util.Locale;
+
+public class DadosNaoPrimitivos {
+    public static void main(String[] args) {
+        // Dados nao primitivos sao basicamente objetos que tem metodos definidos
+
+        String nome = "Rodolfo";
+        System.out.println(nome.toUpperCase());
+
+        Integer num1 = 10;
+        Integer num2 = 15;
+
+
+        System.out.println(num1.compareTo(num2));
+
+
+
+
+
+    }
+}
