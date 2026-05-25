cbb361ea06f4dc28716304e51b15045ff50e54dc
ikeda97 <email@que@voce@usa@no@github.com>
2026-05-24 21:52:29 -0300

    Versao final e simplificada do Desafio01

--- 01-iniciante/src/Desafios/Desafio01.java
+++ 01-iniciante/src/Desafios/Desafio01.java
@@ -3,8 +3,8 @@ package Desafios;
 public class Desafio01 {
     public static void main(String[] args) {
         // Ninja1
-        String nomeNinja1= "Naruto";
-        int idade1 = 15;
+        String nomeNinja1 = "Naruto";
+        int idade1 = 14;
         String nomeMissao1 = "Escolta pais do Arroz";
         char rankMissao1 = 'C';
         boolean missaoConcluida1 = false;
@@ -13,7 +13,7 @@ public class Desafio01 {
         String nomeNinja2 = "Sasuke";
         int idade2 = 15;
         String nomeMissao2 = "Cuidar das criancas na academia";
-        char rankMissao2 = 'D';
+        char rankMissao2 = 'B';
         boolean missaoConcluida2 = false;
 
         //Ninja3
@@ -23,26 +23,37 @@ public class Desafio01 {
         char rankMissao3 = 'D';
         boolean missaoConcluida3 = false;
 
-        if (idade1 < 15 && rankMissao1 == 'C' || rankMissao1 == 'B' || rankMissao1 == 'A'){
-            System.out.println("Missao nao pode ser concluida. Missao completada = " + missaoConcluida1);
+        if (idade1 >= 15) {
+            if (rankMissao1 != 'D' && rankMissao1 != 'C') {
+                System.out.println("Missao nao pode ser concluida. Missao completada = " + missaoConcluida1);
+            } else {
+                missaoConcluida1 = true;
+                System.out.println("A missao foi um sucesso. Missao completada =" + missaoConcluida1);
+            }
         } else {
-            missaoConcluida1 = true;
-            System.out.println("A missao foi um sucesso");
-       }
-
-        if (idade2 < 15 && rankMissao2 == 'C' || rankMissao2 == 'B' || rankMissao2 == 'A'){
-            System.out.println("Missao nao pode ser concluida. Missao completada = " + missaoConcluida1);
-        } else {
-            missaoConcluida2 = true;
-            System.out.println("A missao foi um sucessoMissao completada = " + missaoConcluida2);
+            System.out.println("Esse ninja nao tem idade para sair em Missao");
         }
 
-        if (idade3 < 15 && rankMissao3 == 'C' || rankMissao3 == 'B' || rankMissao3 == 'A'){
-            System.out.println("Missao nao pode ser concluida. Missao completada = " + missaoConcluida1);
+        if (idade2 >= 15) {
+            if (rankMissao2 != 'D' && rankMissao2 != 'C') {
+                System.out.println("Missao nao pode ser concluida. Missao completada= " + missaoConcluida2);
+            } else {
+                missaoConcluida2 = true;
+                System.out.println("A missao foi um sucesso. Missao completa= " + missaoConcluida2);
+            }
         } else {
-            missaoConcluida3 = true;
-            System.out.println("A missao foi um sucessoMissao completada = " + missaoConcluida3);
+            System.out.println("Esse ninja nao tem idade para sair em Missao");
         }
 
+        if (idade3 >= 15) {
+            if (rankMissao3 != 'D' && rankMissao3 != 'C') {
+                System.out.println("Missao nao pode ser concluida. Missao completada= " + missaoConcluida3);
+            } else {
+                missaoConcluida3 = true;
+                System.out.println("A missao foi um sucesso. Missao completa= " + missaoConcluida3);
+            }
+        } else {
+            System.out.println("Esse ninja nao tem idade para sair em Missao");
+        }
     }
 }
